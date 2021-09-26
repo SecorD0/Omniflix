17ae6b0aef5f04d19f772dcb8e04da283827bf15
SecorD0 <vovan6573@gmail.com>
2021-09-26 17:56:14 +0300

    Shows node info

--- node_info.sh
+++ node_info.sh
@@ -1,93 +1,148 @@
 #!/bin/bash
-# $1 - language (RU/EN)
-# $2 - raw JSON output (true/false)
-# Data and variables
-node_tcp=$(cat $HOME/.omniflixhub/config/config.toml | grep -oPm1 "(?<=^laddr = \")([^%]+)(?=\")")
-status=$(omniflixhubd status --node $node_tcp 2>&1)
-moniker=$(jq -r ".NodeInfo.moniker" <<< $status)
-node_info=$(omniflixhubd query staking validators --node $node_tcp --limit 1500 --output json | jq -r '.validators[] | select(.description.moniker=='\"$moniker\"')')
-identity=$(jq -r ".description.identity" <<< $node_info)
-website=$(jq -r ".description.website" <<< $node_info)
-details=$(jq -r ".description.details" <<< $node_info)
-network=$(jq -r ".NodeInfo.network" <<< $status)
-version=$(jq -r ".NodeInfo.version" <<< $status)
-validator_pub_key=$(omniflixhubd tendermint show-validator)
-validator_address=$(jq -r ".operator_address" <<< $node_info)
-jailed=$(jq -r ".jailed" <<< $node_info)
-latest_block_height=$(jq -r ".SyncInfo.latest_block_height" <<< $status)
-catching_up=$(jq -r ".SyncInfo.catching_up" <<< $status)
-delegated=$((`jq -r ".tokens" <<< $node_info`/1000000))
-voting_power=$(jq -r ".ValidatorInfo.VotingPower" <<< $status)
-wallet_address=$(omniflixhubd keys show $omniflix_wallet_name -a)
-# Output
-if [ "$2" = "true" ]; then
-	printf '{"moniker":"%s",
-"identity":"%s",
-"website":"%s",
-"details":"%s",
-"network":"%s",
-"version":"%s",
-"validator_pub_key":"%s",
-"validator_address":"%s",
-"jailed":%b,
-"latest_block_height":%d,
-"catching_up":%b,
-"delegated":%.2f,
-"voting_power":%d}\n' "$moniker" "$identity" "$website" "$details" "$network" "$version" "$validator_pub_key" "$validator_address" "$jailed" "$latest_block_height" "$catching_up" "$delegated" "$voting_power"
+# Default variables
+language="EN"
+raw_output="false"
+# Options
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/colors.sh) --
+option_value(){ echo $1 | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
+while test $# -gt 0; do
+	case "$1" in
+	-h|--help)
+		. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
+		echo
+		echo -e "${C_LGn}Functionality${RES}: the script shows information about OmniFlix node"
+		echo
+		echo -e "Usage: script ${C_LGn}[OPTIONS]${RES}"
+		echo
+		echo -e "You can use ${C_LGn}either${RES} \"=\" or \" \" as an option and value delimiter"
+		echo
+		echo -e "${C_LGn}Options${RES}:"
+		echo -e "  -h, --help               show help page"
+		echo -e "  -l, --language LANGUAGE  use the LANGUAGE for texts"
+		echo -e "                           LANGUAGE is '${C_LGn}EN${RES}' (default), '${C_LGn}RU${RES}'"
+		echo -e "  -ro, --raw-output        the raw JSON output"
+		echo
+		echo -e "${C_LGn}Useful URLs${RES}:"
+		echo -e "https://github.com/SecorD0/Omniflix/blob/main/node_info.sh - script URL (you can send Pull request with new texts to add a language)"
+		echo -e "https://t.me/letskynode — node Community"
+		echo
+		return 0
+		;;
+	-l*|--language*)
+		if ! grep -q "=" <<< $1; then shift; fi
+		language=`option_value $1`
+		shift
+		;;
+	-ro*|--raw-output*)
+		raw_output="true"
+		shift
+		;;
+	*)
+		break
+		;;
+	esac
+done
+# Functions
+printf_n(){ printf "$1\n" "${@:2}"; }
+# Texts
+if [ "$language" = "RU" ]; then
+	t_nn="\nНазвание ноды:                ${C_LGn}%s${RES}"
+	t_id="Keybase ключ:                 ${C_LGn}%s${RES}"
+	t_si="Сайт:                         ${C_LGn}%s${RES}"
+	t_det="Описание:                     ${C_LGn}%s${RES}"
+	t_net="Сеть:                         ${C_LGn}%s${RES}"
+	t_ver="Версия ноды:                  ${C_LGn}%s${RES}\n"
+	t_pk="Публичный ключ валидатора:    ${C_LGn}%s${RES}"
+	t_va="Адрес валидатора:             ${C_LGn}%s${RES}"
+	t_nij1="Нода в тюрьме:                ${C_LR}да${RES}"
+	t_nij2="Нода в тюрьме:                ${C_LGn}нет${RES}"
+	t_lb="Последний блок:               ${C_LGn}%s${RES}"
+	t_sy1="Нода синхронизирована:        ${C_LR}нет${RES}"
+	t_sy2="Нода синхронизирована:        ${C_LGn}да${RES}"
+	t_del="Делегировано токенов на ноду: ${C_LGn}%.3f${RES}"
+	t_vp="Весомость голоса:             ${C_LGn}%s${RES}\n"
+	t_wa="Адрес кошелька:               ${C_LGn}%s${RES}"
+	t_bal="Баланс:                       ${C_LGn}%.3f${RES}\n"
+# Send Pull request with new texts to add a language - https://github.com/SecorD0/Omniflix/blob/main/node_info.sh
+#elif [ "$language" = ".." ]; then
 else
-	if [ "$1" = "RU" ]; then
-		echo -e ""
-		echo -e "Название ноды:                \e[40m\e[92m$moniker\e[0m"
-		echo -e "Keybase ключ:                 \e[40m\e[92m$identity\e[0m"
-		echo -e "Сайт:                         \e[40m\e[92m$website\e[0m"
-		echo -e "Описание:                     \e[40m\e[92m$details\e[0m"
-		echo -e "Сеть:                         \e[40m\e[92m$network\e[0m"
-		echo -e "Версия ноды:                  \e[40m\e[92m$version\e[0m"
-		echo -e ""
-		echo -e "Публичный ключ валидатора:    \e[40m\e[92m$validator_pub_key\e[0m"
-		echo -e "Адрес валидатора:             \e[40m\e[92m$validator_address\e[0m"
-		if [ "$jailed" = "true" ]; then
-			echo -e "Нода в тюрьме:                \033[0;31mда\e[0m"
-		else
-			echo -e "Нода в тюрьме:                \e[40m\e[92mнет\e[0m"
-		fi
-		echo -e "Последний блок:               \e[40m\e[92m$latest_block_height\e[0m"
-		if [ "$catching_up" = "true" ]; then
-			echo -e "Нода синхронизирована:        \033[0;31mнет\e[0m"
-		else
-			echo -e "Нода синхронизирована:        \e[40m\e[92mда\e[0m"
-		fi
-		echo -e "Делегировано токенов на ноду: \e[40m\e[92m$delegated\e[0m"
-		echo -e "Весомость голоса:             \e[40m\e[92m$voting_power\e[0m"
-		echo -e ""
-		echo -e "Адрес кошелька:               \e[40m\e[92m$wallet_address\e[0m"
-		echo -e ""
+	t_nn="\nMoniker:                       ${C_LGn}%s${RES}"
+	t_id="Keybase key:                   ${C_LGn}%s${RES}"
+	t_si="Website:                       ${C_LGn}%s${RES}"
+	t_det="Details:                       ${C_LGn}%s${RES}"
+	t_net="Network:                       ${C_LGn}%s${RES}"
+	t_ver="Node version:                  ${C_LGn}%s${RES}\n"
+	t_pk="Validator public key:          ${C_LGn}%s${RES}"
+	t_va="Validator address:             ${C_LGn}%s${RES}"
+	t_nij1="The node in a jail:            ${C_LR}yes${RES}"
+	t_nij2="The node in a jail:            ${C_LGn}no${RES}"
+	t_lb="Latest block height:           ${C_LGn}%s${RES}"
+	t_sy1="The node is synchronized:      ${C_LR}no${RES}"
+	t_sy2="The node is synchronized:      ${C_LGn}yes${RES}"
+	t_del="Delegated tokens to the node:  ${C_LGn}%.3f${RES}"
+	t_vp="Voting power:                  ${C_LGn}%s${RES}\n"
+	t_wa="Wallet address:                ${C_LGn}%s${RES}"
+	t_bal="Balance:                       ${C_LGn}%.3f${RES}\n"
+fi
+# Actions
+sudo apt install bc -y &>/dev/null
+node_tcp=`cat $HOME/.omniflixhub/config/config.toml | grep -oPm1 "(?<=^laddr = \")([^%]+)(?=\")"`
+status=`omniflixhubd status --node "$node_tcp" 2>&1`
+moniker=`jq -r ".NodeInfo.moniker" <<< $status`
+node_info=`omniflixhubd query staking validators --node "$node_tcp" --limit 1500 --output json | jq -r '.validators[] | select(.description.moniker=='\"$moniker\"')'`
+identity=`jq -r ".description.identity" <<< $node_info`
+website=`jq -r ".description.website" <<< $node_info`
+details=`jq -r ".description.details" <<< $node_info`
+network=`jq -r ".NodeInfo.network" <<< $status`
+version=`jq -r ".NodeInfo.version" <<< $status`
+validator_pub_key=`omniflixhubd tendermint show-validator | sed "s%\"%'%g"`
+validator_address=`jq -r ".operator_address" <<< $node_info`
+jailed=`jq -r ".jailed" <<< $node_info`
+latest_block_height=`jq -r ".SyncInfo.latest_block_height" <<< $status`
+catching_up=`jq -r ".SyncInfo.catching_up" <<< $status`
+delegated=`echo "$(jq -r ".tokens" <<< $node_info)/1000000" | bc -l`
+voting_power=`jq -r ".ValidatorInfo.VotingPower" <<< $status`
+wallet_address=`omniflixhubd keys show $omniflix_wallet_name -a`
+balance=`echo "$(omniflixhubd query bank balances $wallet_address -o json --node "$node_tcp" | jq -r ".balances[0].amount")/1000000" | bc -l`
+if [ "$raw_output" = "true" ]; then
+	printf '{"moniker": "%s", "identity": "%s", "website": "%s", "details": "%s", "network": "%s", "version": "%s", "validator_pub_key": "%s", "validator_address": "%s", "jailed": %b, "latest_block_height": %d, "catching_up": %b, "delegated": %.3f, "voting_power": %d, "wallet_address": "%s", "balance": %.3f}\n' \
+"$moniker" \
+"$identity" \
+"$website" \
+"$details" \
+"$network" \
+"$version" \
+"$validator_pub_key" \
+"$validator_address" \
+"$jailed" \
+"$latest_block_height" \
+"$catching_up" \
+"$delegated" \
+"$voting_power" \
+"$wallet_address" \
+"$balance"
+else
+	printf_n "$t_nn" "$moniker"
+	printf_n "$t_id" "$identity"
+	printf_n "$t_si" "$website"
+	printf_n "$t_det" "$details"
+	printf_n "$t_net" "$network"
+	printf_n "$t_ver" "$version"
+	printf_n "$t_pk" "$validator_pub_key"
+	printf_n "$t_va" "$validator_address"
+	if [ "$jailed" = "true" ]; then
+		printf_n "$t_nij1"
 	else
-		echo -e ""
-		echo -e "Moniker:                       \e[40m\e[92m$moniker\e[0m"
-		echo -e "Keybase key:                   \e[40m\e[92m$identity\e[0m"
-		echo -e "Website:                       \e[40m\e[92m$website\e[0m"
-		echo -e "Details:                       \e[40m\e[92m$details\e[0m"
-		echo -e "Network:                       \e[40m\e[92m$network\e[0m"
-		echo -e "Node version:                  \e[40m\e[92m$version\e[0m"
-		echo -e ""
-		echo -e "Validator public key:          \e[40m\e[92m$validator_pub_key\e[0m"
-		echo -e "Validator address:             \e[40m\e[92m$validator_address\e[0m"
-		if [ "$jailed" = "true" ]; then
-			echo -e "The node in a jail:            \033[0;31myes\e[0m\n"
-		else
-			echo -e "The node in a jail:            \e[40m\e[92mno\e[0m"
-		fi
-		echo -e "Latest block height:           \e[40m\e[92m$latest_block_height\e[0m"
-		if [ "$catching_up" = "true" ]; then
-			echo -e "The node is synchronized:      \033[0;31mno\e[0m"
-		else
-			echo -e "The node is synchronized:      \e[40m\e[92myes\e[0m"
-		fi
-		echo -e "Delegated tokens to the node:  \e[40m\e[92m$delegated\e[0m"
-		echo -e "Voting power:                  \e[40m\e[92m$voting_power\e[0m"
-		echo -e ""
-		echo -e "Wallet address:               \e[40m\e[92m$wallet_address\e[0m"
-		echo -e ""
+		printf_n "$t_nij2"
 	fi
+	printf_n "$t_lb" "$latest_block_height"
+	if [ "$catching_up" = "true" ]; then
+		printf_n "$t_sy1"
+	else
+		printf_n "$t_sy2"
+	fi
+	printf_n "$t_del" "$delegated"
+	printf_n "$t_vp" "$voting_power"
+	printf_n "$t_wa" "$wallet_address"
+	printf_n "$t_bal" "$balance"
 fi
